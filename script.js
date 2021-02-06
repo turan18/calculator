d78aad4a0971289df62c11c2d1fa90f3fc01a047
Yasin <yasinturan888@gmail.com>
2021-02-06 16:16:32 -0500

    Numbers/operations are now displayed in preview

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 
-let buttons = Array.from(document.querySelectorAll('.num'));
-const validKeys = [];
-buttons.forEach(button=>{
-    validKeys.push(button.getAttribute("data-key"));    
-})
+
 
 let counter = 0;
-document.addEventListener('keydown',function(e){
-    
-    if(e.key==="."){
-        counter++;
-    }
-    if(validKeys.includes(e.keyCode.toString())){
-        let value = document.createTextNode(e.key);
-        if(e.key==="." && counter>1){
-            //
-        }
-        else{
-            document.querySelector('.display-ans').appendChild(value);
-        }
-        
-    }
-})
-buttons.forEach(button=>{
+Array.from(document.querySelectorAll('.num')).forEach(button=>{
     button.addEventListener('click',function(){
         let value = document.createTextNode(String.fromCharCode(button.getAttribute("data-key")));
-        if(String.fromCharCode(button.getAttribute("data-key"))==="."){
-            
+        if(String.fromCharCode(button.getAttribute("data-key"))==="."){            
             counter++;
-        
         }
-        if(String.fromCharCode(button.getAttribute("data-key")) && counter>1){
-            //
-        }
-        else{
+        if(!(String.fromCharCode(button.getAttribute("data-key"))==="." && counter>1)){
             document.querySelector('.display-ans').appendChild(value);
         }
-        
-        
     });
 });
+
+Array.from(document.querySelectorAll('.operator')).forEach(operation=>{
+    operation.addEventListener('click',()=>{
+        if(operation.getAttribute('id')=='sign'){
+            let sign = (document.querySelector('.sign').textContent).replace(/\s/g, "");
+            if(sign===''){
+                document.querySelector('.sign').textContent = '-'
+            }
+            else{
+                document.querySelector('.sign').textContent = ''
+            }
+        }
+        if(operation.getAttribute('id')=='divide'){
+            display(String.fromCharCode(operation.getAttribute("data-key")));
+
+        }
+        if(operation.getAttribute('id')=='mult'){
+            display(String.fromCharCode(operation.getAttribute("data-key")));
+           
+        }
+        if(operation.getAttribute('id')=='subtract'){
+            display(String.fromCharCode(operation.getAttribute("data-key")));
+
+        }
+        if(operation.getAttribute('id')=='add'){
+            display(String.fromCharCode(operation.getAttribute("data-key")));
+
+        }
+    })
+})
+function display(op){
+    let number = (document.querySelector('.answer').textContent).replace(/\s/g, "");
+    if(number.indexOf('.') + 1 === number.length){
+        number = number.replace('.','')
+    }
+    let dispreview = document.createTextNode(number + op);
+    document.querySelector('.sign').textContent = ''; document.querySelector('.display-ans').textContent = '';  
+    if(number!==''){
+        document.querySelector('.equation').appendChild(dispreview);
+    }
+    counter=0; 
+}
+
